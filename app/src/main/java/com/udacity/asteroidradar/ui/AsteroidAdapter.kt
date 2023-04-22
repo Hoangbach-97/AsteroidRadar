3feee3748266fc5e51a9aa6ae067f2fa3e633947
hoangbach <petxuanbach@gmail.com>
2023-04-22 21:02:49 +0700

    Create structure for project

---
+++ app/src/main/java/com/udacity/asteroidradar/ui/AsteroidAdapter.kt
@@ -0,0 +1,26 @@
+package com.udacity.asteroidradar.ui
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AsteroidAdapter : RecyclerView.Adapter<AsteroidAdapter.AsteroidHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AsteroidHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: AsteroidHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+    class AsteroidHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+}
