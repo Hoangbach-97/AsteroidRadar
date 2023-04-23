5a1cc262ea8a4a3fa5659bf2b0d5e96865653933
hoangbach <petxuanbach@gmail.com>
2023-04-23 15:13:33 +0700

    Project 2 - day 2

--- app/src/main/java/com/udacity/asteroidradar/network/AsteroidApiService.kt
+++ app/src/main/java/com/udacity/asteroidradar/network/AsteroidApiService.kt
@@ -1,22 +1,51 @@
 package com.udacity.asteroidradar.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.domain.Asteroid
+import com.udacity.asteroidradar.domain.PictureOfDay
+import com.udacity.asteroidradar.util.Constants.API_KEY
 import com.udacity.asteroidradar.util.Constants.BASE_URL
+import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 // URL for Asteroid: https://api.nasa.gov/neo/rest/v1/feed?start_date=START_DATE&end_date=END_DATE&api_key=YOUR_API_KEY
 // URL for NASA image of the day: https://api.nasa.gov/planetary/apod?api_key=YOUR_API_KEY
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+//    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 
-
 interface AsteroidApiService {
 
+    @GET("neo/rest/v1/feed?")
+    suspend fun getAsteroidProperties(
+        @Query("start_date") startDate: String,
+        @Query("end_date") endDate: String,
+        @Query("api_key") apiKey: String = API_KEY
+    ): List<Asteroid>
+
+    @GET("planetary/apod?")
+    suspend fun getPlanetaty(
+        @Query("api_key") apiKey: String = API_KEY
+    ): PictureOfDay
+
+}
+
+object AsteroidApi {
+    val retrofitService: AsteroidApiService by lazy {
+        retrofit.create(AsteroidApiService::class.java)
+    }
 }
--- app/src/main/java/com/udacity/asteroidradar/repo/AsteroidRepo.kt
+++ app/src/main/java/com/udacity/asteroidradar/repo/AsteroidRepo.kt
@@ -1,4 +1,15 @@
 package com.udacity.asteroidradar.repo
 
+import com.udacity.asteroidradar.domain.Asteroid
+
 class AsteroidRepo {
+
+    fun getDataAsteroid(){
+
+    }
+
+    fun saveDataAsteroidToLocal(asteroidList: List<Asteroid>): Boolean{
+
+        return true
+    }
 }
